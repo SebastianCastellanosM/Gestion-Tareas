ec0a029c8423ffca649962a8832f54624a93923d
SebastianCastellanosM <scastellanos.mejia@udea.edu.co>
2024-11-20 14:57:04 -0500

    sidebar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home({ users }: any) {
   export default function Home({ users }: any) {
       return (
         <div
-          className={`grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+          className={`flex h-screen bg-white`}
         >
     
           <Sidebar />
